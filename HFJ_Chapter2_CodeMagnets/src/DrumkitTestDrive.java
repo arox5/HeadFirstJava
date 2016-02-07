03bf004dc5d26e8c3f223337fcf06ed9d6da12cb
Andrea <Andrea@192.168.56.1>
2016-02-07 19:22:40 +0100

    prova 3

---
+++ HFJ_Chapter2_CodeMagnets/src/DrumkitTestDrive.java
@@ -0,0 +1,17 @@
+
+public class DrumkitTestDrive {
+	public static void main(String[] args){
+		Drumkit d = new Drumkit();
+		
+		d.playSnare();
+		
+		d.snare = false;
+		
+		if (d.snare == true) {
+			d.playSnare();
+		}
+		
+		d.playTopHat();
+	}
+
+}
